47204ff7cbc61ee59e02f458a062ddb2d9ce1ef9
mrevilhunter2-pixel <mrevilhunter2@gmail.com>
2026-09-06 21:56:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, request, jsonify, session, redirect
 import os
 import random
 from werkzeug.utils import secure_filename
+import cloudinary
+import cloudinary.uploader
+
+# Cloudinary Config
+cloudinary.config(
+    cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key = os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret = os.environ.get('CLOUDINARY_API_SECRET')
+)
+
 
 # Database Selection: Render par Supabase PostgreSQL, Local me SQLite
 DATABASE_URL = os.environ.get('DATABASE_URL')
